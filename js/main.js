e333d3363ec710ed7c8ab6d71b0f4b43f9077821
tarasbzovskyy <t.bzovskyy@gmail.com>
2017-06-08 02:34:09 +0300

    from home

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 
+(function($){
+    $(window).load(function() {
+        $(".product-photo-thumbs")
+            .flexslider({
+                animation: "fade",
+                controlNav: false,
+                animationLoop: false,
+                slideshow: false,
+                itemWidth: 66,
+                itemMargin: 5,
+                asNavFor: '.product-photo'
+
+            });
+        $(".product-photo")
+            .flexslider({
+                animation: "fade",
+                controlNav: false,
+                animationLoop: false,
+                slideshow: false,
+                sync: ".product-photo-thumbs"
+            });
+
+    });
+})(jQuery);
