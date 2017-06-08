77ff7b6228ce4c84521295f5c3bffb35179f08fa
tarasbzovskyy <t.bzovskyy@gmail.com>
2017-06-08 20:21:19 +0300

    from job with <3

--- js/main.js
+++ js/main.js
@@ -21,5 +21,46 @@
                 sync: ".product-photo-thumbs"
             });
 
+        function productSlider(target){
+            var slider = target;
+            slider.owlCarousel({
+                items:5,
+                margin:10,
+                nav:false,
+                touchDrag:true,
+                dots:false,
+                // navSpeed:1000,
+                responsiveClass:true,
+                // responsiveRefreshRate:1000,
+                responsive:{
+                    0:{
+                        items:1,
+                        nav:true
+                    },
+                    600:{
+                        items:3,
+                        nav:false
+                    },
+                    1000:{
+                        items:5,
+                        nav:true,
+                        loop:false
+                    }
+                }
+            });
+
+            $('.slider-nav .next').on('click',function(){
+                $(slider).trigger('next.owl.carousel');
+            });
+            $('.slider-nav .prev').on('click',function(){
+                $(slider).trigger('prev.owl.carousel');
+            })
+        }
+
+        productSlider($(".product-slider .slides"));
+
     });
+
+
+
 })(jQuery);
